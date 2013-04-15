3bfe65bc445c5045c73042e5c94c23d6210ab49b
Mathias Buus <mathiasbuus@gmail.com>
2013-04-15 23:41:31 +0200

    more aggresive peer discovery and reconnecting all prev connected peers

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
-// TODO: refactor this completely!
-
-var dgram = require('dgram');
-var crypto = require('crypto');
-var net = require('net');
 var EventEmitter = require('events').EventEmitter;
+var parseUrl = require('url').parse;
+var crypto = require('crypto');
+var dgram = require('dgram');
+var net = require('net');
+var once = require('once');
+var compact2string = require('compact2string');
 var bncode = require('bncode');
 
-var inherits = require('util').inherits;
+var MAX_NODES = 5000;
+var TIMEOUT = 10000;
 
-var RANDOM_BYTES = crypto.randomBytes(20*1024);
+var randomId = function() {
+	var bytes = crypto.randomBytes(2000);
+	var offset = 0;
 
-var DEFAULT_NODES = [{
-	host: 'router.bittorrent.com',
-	port: 6881
-},{
-	host: 'router.utorrent.com',
-	port: 6881
-},{
-	host: 'dht.transmissionbt.com',
-	port: 6881
-}];
+	return function() {
+		var id = bytes.slice(offset, offset + 20);
+		offset = (offset + 20) % bytes.length;
+		return id;
+	};
+}();
 
 var parseNodeInfo = function(compact) {
-	var nodes = [];
-
-	for (var i = 0; i < compact.length; i += 26) {
-		nodes.push({
-			id: compact.slice(i, i+20),
-			host: compact[20+i]+'.'+compact[i+21]+'.'+compact[i+22]+'.'+compact[i+23],
-			port: compact.readUInt16BE(i+24)
-		});
-	};
-
-	return nodes;
+	try {
+		var nodes = [];
+		for (var i = 0; i < compact.length; i += 26) {
+			nodes.push(compact2string(compact.slice(i+20, i+26)));
+		}
+		return nodes;
+	} catch(err) {
+		return [];
+	}
 };
 
 var parsePeerInfo = function(list) {
-	return list.map(function(peer) {
-		return {
-			host: peer[0]+'.'+peer[1]+'.'+peer[2]+'.'+peer[3],
-			port: peer.readUInt16BE(4)
-		};
-	});
+	try {
+		return list.map(compact2string);
+	} catch (err) {
+		return [];
+	}
 };
-
-var randomOffset = 0;
 
 var Swarm = function(infoHash, options, onconnection) {
 	if (!(this instanceof Swarm)) return new Swarm(infoHash, options, onconnection);
-
 	EventEmitter.call(this);
 
 	if (typeof options === 'function') {
@@ -58,187 +52,143 @@ var Swarm = function(infoHash, options, onconnection) {
 
 	options = options || {};
 
-	this.infoHash = typeof infoHash === 'string' ? new Buffer(infoHash, 'hex') : infoHash;
-	this.maxSize = options.maxSize || 30;
-	this.maxNodes = options.maxNodes || 2500;
+	if (typeof infoHash === 'string') infoHash = new Buffer(infoHash, 'hex');
 
-	this.connections = [];
+	this.maxSize = options.maxSize || 60;
+	this.infoHash = infoHash;
+	this.nodeId = randomId();
 
-	this._nextPeers = [];
-	this._nextNodes = [];
-	this._discoveredPeers = {};
-	this._discoveredNodes = {};
-	this._nodeId = RANDOM_BYTES.slice(randomOffset, randomOffset+20);
-	this._reconnects = {};
-	this._reconnecting = {};
-
-	randomOffset = (randomOffset + 20) % RANDOM_BYTES.length;
-
-	this._dht = dgram.createSocket('udp4');
 	this._t = 0;
-	this._sent = {};
-	this._bootstrap = options.nodes || DEFAULT_NODES;
+	this._sock = dgram.createSocket('udp4');
+	this._visitedNodes = {};
+	this._visitedPeers = {};
 
-	if (onconnection) this.on('connection', onconnection);
-};
+	this.queue = [];
+	this.connections = [];
+	this.prioritized = {};
+	this.reconnecting = {};
 
-inherits(Swarm, EventEmitter);
-
-Swarm.prototype.__defineGetter__('nodesFound', function() {
-	return Object.keys(this._discoveredNodes).length;
-});
-
-Swarm.prototype.__defineGetter__('peersFound', function() {
-	return Object.keys(this._discoveredPeers).length;
-});
-
-Swarm.prototype.__defineGetter__('queued', function() {
-	return this._nextPeers.length;
-});
-
-Swarm.prototype.reconnect = function(peer) {
-	if (typeof peer === 'string') return this.reconnect({host:peer.split(':')[0], port:parseInt(peer.split(':')[1],10)});
-	var id = peer.host+':'+peer.port;
-	this._reconnects[id] = 0;
-	this._reconnect(id, peer);
-};
-
-Swarm.prototype.addPeer = function(peer) {
-	var id = peer.host+':'+peer.port;
-
-	if (this._discoveredPeers[id]) return this._findPeers();
-	this._discoveredPeers[id] = true;
-	this._nextPeers.push(peer);
-	this._connectPeers();
-};
-
-Swarm.prototype.addNode = function(node) {
-	var id = node.id || node.host+':'+node.port;
-
-	if (Object.keys(this._discoveredNodes).length >= this.maxNodes) return this._findPeers();
-	if (this._discoveredNodes[id]) return this._findPeers();
-
-	this._discoveredNodes[id] = {time:Date.now(), node:node};
-	this._nextNodes.push(node);
-	this._findPeers();
-};
-
-Swarm.prototype.listen = function() {
 	var self = this;
-	var addPeer = this.addPeer.bind(this);
-	var addNode = this.addNode.bind(this);
+	var node = this.node.bind(this);
+	var peer = this.peer.bind(this);
 
-	this._dht.on('error', function(err) {
-		// do nothing...
+	this._sock.on('error', function() {
+		// ... ignore error ...
 	});
+	this._sock.on('message', function(message, remote) {
+		self._visitedNodes[remote.address+':'+remote.port] = true;
 
-	this._dht.on('message', function(message) {
 		try {
 			message = bncode.decode(message);
 		} catch (err) {
 			return;
 		}
 
-		var r = message.y.toString() === 'r' && message.r;
-		if (!r) return;
+		var nodes = message.r && message.r.nodes;
+		var values = message.r && message.r.values;
 
-		delete self._sent[message.t.toString()];
-
-		var values = r && r.values && parsePeerInfo(r.values);
-		if (values) values.forEach(addPeer);
-
-		var nodes = r && r.nodes && parseNodeInfo(r.nodes);
-		if (nodes) nodes.forEach(addNode);
+		if (nodes) parseNodeInfo(nodes).forEach(node);
+		if (values) parsePeerInfo(values).forEach(peer);
 	});
 
-	var gc = function() {
-		var now = Date.now();
+	if (onconnection) this.on('connection', onconnection);
+};
 
-		Object.keys(self._sent).forEach(function(t) {
-			var sent = self._sent[t];
+Swarm.prototype.__proto__ = EventEmitter.prototype;
 
-			if (now - sent.time < self.tries * 2500) return;
-			if (sent.tries >= 5) return delete self._sent[t];
-
-			sent.tries++;
-			self._dht.send(sent.message, 0, sent.message.length, sent.node.port, sent.node.host);
-		});
-	};
-
-	var reset = function() {
-		var nodes = self._discoveredNodes;
-		var now = Date.now();
-
-		Object.keys(nodes).some(function(id) {
-			if (now - nodes[id].time < 90000) return true;
-			var node = nodes[id].node;
-			delete nodes[id];
-			self.addNode(node);
-		});
-	};
-
-	setInterval(gc, 2000);
-	setInterval(reset, 10000);
-	this._bootstrap.forEach(addNode);
+Swarm.prototype.listen = function() {
+	this.node('router.bittorrent.com:6881');
+	this.node('router.utorrent.com:6881');
+	this.node('dht.transmissionbt.com:6881');
 	return this;
 };
 
-Swarm.prototype._connectPeers = function() {
-	this._findPeers(); // find more peers!
+Swarm.prototype.connect = function(peer) {
+	if (this.prioritized[peer]) {
+		this.queue.unshift(peer);
+	} else {
+		this.queue.push(peer);
+	}
 
-	if (this.connections.length >= this.maxSize || !this._nextPeers.length) return;
-
-	var self = this;
-	var peer = this._nextPeers.pop();
-	var socket = net.connect(peer.port, peer.host, {allowHalfOpen:false});
-	var id = peer.host+':'+peer.port;
-
-	var onend = function() {
-		var index = self.connections.indexOf(socket);
-		if (index > -1) self.connections.splice(index, 1);
-		if (index > -1 && self._reconnects[id]) self._reconnect(id, peer);
-		self._connectPeers();
-	};
-
-	var timeout = setTimeout(socket.destroy.bind(socket), 4000);
-
-	socket.on('error', onend);
-	socket.on('end', onend);
-	socket.on('close', onend);
-
-	self.connections.push(socket);
-
-	socket.once('connect', function() {
-		clearTimeout(timeout);
-		self.emit('connection', socket, id, peer);
-	});
+	this._connect();
 };
 
-Swarm.prototype._findPeers = function() {
-	if (this._nextPeers.length > 2*this.maxSize || !this._nextNodes.length) return;
-	if (Object.keys(this._sent).length > Math.max(this.maxSize - this._nextPeers.length, 5)) return;
+Swarm.prototype.reconnect = function(peer) {
+	this.prioritized[peer] = true;
+	this._connect();
+};
 
-	var node = this._nextNodes.pop();
+Swarm.prototype.peer = function(peer) {
+	if (this._visitedPeers[peer]) return;
+	this._visitedPeers[peer] = true;
+	this.connect(peer);
+};
+
+Swarm.prototype.node = function(addr) {
+	if (this._visitedNodes[addr]) return;
+	if (Object.keys(this._visitedNodes).length >= MAX_NODES) return;
+
 	var t = ''+(this._t++);
-	var message = bncode.encode({t:t, y:'q', q:'get_peers', a:{id:this._nodeId, info_hash:this.infoHash}});
+	var message = bncode.encode({t:t,y:'q',q:'get_peers',a:{id:this.nodeId,info_hash:this.infoHash}});
+	var port = addr.split(':')[1];
+	var host = addr.split(':')[0];
 
-	this._dht.send(message, 0, message.length, node.port, node.host);
-	this._sent[t] = {node:node, message:message, time:Date.now(), tries:1};
+	this._sock.send(message, 0, message.length, port, host);
 };
 
-
-Swarm.prototype._reconnect = function(id, peer) {
-	if (this._reconnecting[id]) return;
+Swarm.prototype._connect = function() {
+	if (!this.queue.length || this.connections.length >= this.maxSize) return;
 
 	var self = this;
-	var reconnect = function() {
-		self._reconnecting[id] = null;
-		self._nextPeers.push(peer);
-		self._connectPeers();
+	var peer = this.queue.shift();
+	var port = peer.split(':')[1];
+	var host = peer.split(':')[0];
+
+	var socket = net.connect(port, host);
+
+	var onclose = once(function() {
+		self.connections.splice(self.connections.indexOf(socket), 1);
+		self._connect();
+		if (!connected) return;
+		self.reconnecting[peer]++;
+		setTimeout(self.connect.bind(self, peer), self.reconnecting[peer] * 5000);
+	});
+
+	socket.on('error', onclose);
+	socket.on('close', onclose);
+	socket.on('end', function() {
+		socket.end(); // no half open
+		onclose();
+	});
+
+	var ontimeout = function() {
+		socket.destroy();
 	};
 
-	this._reconnects[id] = (this._reconnects[id] || 0) + 1;
-	this._reconnecting[id] = setTimeout(reconnect, this._reconnects[id] * 5000);
+	var timeout = setTimeout(ontimeout, TIMEOUT);
+	var connected = false;
+
+	socket.on('connect', function() {
+		connected = true;
+		clearTimeout(timeout);
+		self.reconnecting[peer] = 0;
+		self.emit('connection', socket, peer, peer);
+	});
+
+	this.connections.push(socket);
 };
+
+// TODO: remove me
+Swarm.prototype.__defineGetter__('peersFound', function() {
+	return Object.keys(this._visitedPeers).length;
+});
+
+Swarm.prototype.__defineGetter__('nodesFound', function() {
+	return Object.keys(this._visitedNodes).length;
+});
+
+Swarm.prototype.__defineGetter__('queued', function() {
+	return this.queue.length;
+});
 
 module.exports = Swarm;
